f8bcf82deb7da7f14929c047d25fd72ddd463368
Fraser McGhan <mcghanf@cardiff.ac.uk>
2021-02-19 19:18:20 +0000

    initial commit - Env working example.

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import habitat
+
+
+with habitat.Env(config=habitat.get_config("pointnav.yaml")) as env:
+    print("Environment creation successful")
+    observations = env.reset()
+
+    print("Agent stepping around inside environment.")
+    count_steps = 0
+    while not env.episode_over:
+        observations = env.step(env.action_space.sample())  # noqa: F841
+        count_steps += 1
+    print("Episode finished after {} steps.".format(count_steps))
